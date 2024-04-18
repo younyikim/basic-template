d79015a48cef84405cbae956d881a1562db32f2d
Younyi Kim <rlakuku1221@gmail.com>
2024-04-18 23:15:14 +0900

    fix: commonJS -> module

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	printWidth: 100,
+	tabWidth: 2,
+	singleQuote: true,
+	trailingComma: 'all',
+	bracketSpacing: true, // [1,2] -> [ 1, 2 ]
+	semi: true,
+	useTabs: true,
+	arrowParens: 'avoid', // (x) => x를 x => x로 변환
+	endOfLine: 'lf',
+	plugins: ['prettier-plugin-tailwindcss'],
+};
