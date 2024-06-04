24e226d7bfd07c9788359b386824270c73b6b8fb
Younyi Kim <rlakuku1221@gmail.com>
2024-06-04 11:16:56 +0900

    feat : TanStack Query 추가

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import reactLogo from '@assets/react.svg';
-import viteLogo from '@public/vite.svg';
 import '@/App.css';
 
 // Components
@@ -12,14 +10,6 @@ function Root() {
 
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
 				<button onClick={() => setCount(count => count + 1)}>count is {count}</button>
